71b67890171b8c9102d64e8d5ac26e7fc5583379
npbehunin <npbehunin@gmail.com>
2019-04-08 18:28:36 -0600

    Added 16x16 blocks and player movement script tests

---
+++ 2D Adventure Game/Assets/Scripts/PlayerRigidbodyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRigidbodyMovement : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+	public Animator animator;
+
+	private float horizontalspeed;
+	private float verticalspeed;
+
+	public float MoveSpeed;
+	public Vector3 position;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		MoveSpeed = .06f;
+
+		animator = gameObject.GetComponent<Animator>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(transform.position + position);
+	}
+
+	void Update()
+	{
+		position.Set((MoveSpeed * Input.GetAxisRaw("Horizontal")), (MoveSpeed * Input.GetAxisRaw("Vertical")), 0);
+
+		Debug.Log(position);
+		horizontalspeed = position.x;
+		verticalspeed = position.y;
+
+		animator.SetFloat("SpeedX", horizontalspeed);
+		animator.SetFloat("SpeedY", verticalspeed);
+	}
+}
