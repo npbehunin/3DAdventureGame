150b479d3e87c18e9e4f677532be21f7d5b944cc
npbehunin <npbehunin@gmail.com>
2019-08-28 19:59:06 -0600

    Added Kinematic Character controller assets and began editing
    Started learning the code and writing down notes. Adjusted a few things we won't need and created some example scenes for testing purposes. Fixed the air movement and began custom acceleration.

---
+++ 3D Adventure Game New/Assets/KinematicCharacterController/Testing (Nate)/CHARCONTROLLER NOTES.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHARCONTROLLERNOTES : MonoBehaviour
+{
+    //We're using this script to write down all information and notes we want to take on the Character Controller tutorial.
+    
+    //
+}
